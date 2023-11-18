939a9c0980a6c3fba93d3b4164338832809f0e7b
krk <72791356+scrooge001@users.noreply.github.com>
2023-11-18 11:48:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -137,8 +137,8 @@ def list_to_str(k):
     else:
         return ' '.join(f'{elem}, ' for elem in k)
 
-__repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
-__version__ = "PROFESSOR-BOT ᴠ4.5.0"
+__repo__ = "https://github.com/scrooge001/ADGFILTERV3"
+__version__ = "ADG Filter ᴠ4"
 __license__ = "GNU GENERAL PUBLIC LICENSE V2"
 __copyright__ = "Copyright (C) 2023-present ADG MOVIES <t.me/adgmovies>"
 
